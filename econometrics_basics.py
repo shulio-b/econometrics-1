257aba89c8e29c71f07cd0e244ff42334a8ac720
shulio-b <julio.balena.tech@gmail.com>
2024-03-27 21:15:33 -0300

    correcting some equations

--- econometrics_basics.py
+++ econometrics_basics.py
@@ -27,6 +27,12 @@ class Econometrics:
     def xi_minus_average_x(self):
         return [xi - self.x_mean for xi in self.x]
 
+    def sum_xi_minus_average_x(self):
+        return sum((xi - self.x_mean) for xi in self.x)
+    
+    def square_sum_xi_minus_average_x(self):
+        return sum((xi - self.x_mean) ** 2 for xi in self.x)
+
     def yi_minus_average_y(self):
         return [yi - self.y_mean for yi in self.y]
 
@@ -51,6 +57,9 @@ class Econometrics:
     def r_squared(self):
         return 1 - (self.sse / self.sst)
 
+    def variance_beta1(self):
+        return self.sse / self.square_sum_xi_minus_average_x()
+
     def critical_value(self, alpha=0.05):
         df = len(self.x) - 2  # degrees of freedom
         return stats.t.ppf(1 - alpha / 2, df)
@@ -60,12 +69,15 @@ x = [59, 71, 70, 64, 72, 73, 64, 70, 68, 75]
 y = [165, 167, 175, 179, 170, 169, 164, 173, 174, 175]
 
 model = Econometrics(x, y)
-#print("Imported table:", model.import_table())
+print("Imported table:", model.import_table())
 print("Beta0:", model.beta0)
 print("Beta1:", model.beta1)
-#print("y^ (predicted y):", model.y_hat)
+print("y^ (predicted y):", model.y_hat)
 print("SSE (Sum of Squared Errors):", model.sse)
 print("SST (Total Sum of Squares):", model.sst)
 print("SSR (Regression Sum of Squares):", model.ssr)
 print("R² (Coefficient of Determination):", model.r_squared())
+print("Sum of (xi - mean x):", model.sum_xi_minus_average_x())
+print("Square sum of (xi - mean x)", model.square_sum_xi_minus_average_x())
+print("Variance of Beta1:", model.variance_beta1())
 print("Critical value (t-distribution):", model.critical_value())
